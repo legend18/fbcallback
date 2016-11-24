7a9307ca43be8be144c7f2fa18fabe20625e6bb7
legend18 <baiyu@boyunet.local>
2016-11-24 10:49:49 +0800

    2
    2

--- payments/config.php
+++ payments/config.php
@@ -7,7 +7,7 @@
     - create a new App and retrieve the app ID and namespace from Facebook
     Developer site at https://developers.facebook.com/apps
 */
-  $serverUrl = "http://slot0.78games.com";
+  $serverUrl = "https://github.com/legend18/fbcallback";
   $appNamespace = "bynetslot";
   $appId = "155901198183075";
 ?>
